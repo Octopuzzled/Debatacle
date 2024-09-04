b0f1d44518d17d7b576d19676990d986ace25aea
Octopuzzled <wyld@vivaldi.net>
2024-09-04 20:37:32 +0200

    First steps layout, index and app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Debug mode activated!
+app.config['DEBUG'] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# Run the flask app
+if __name__ == "__main__":
+    app.run()
